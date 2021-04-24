6c0caa0fee31726a04107498c2375861a15250ff
megabspam <megabspam@yandex.ru>
2021-04-24 18:34:26 +0700

    fix2

--- Sql2csv/Program.cs
+++ Sql2csv/Program.cs
@@ -13,7 +13,6 @@ namespace Sql2csv
         static void Main(string[] args)
         {
             StreamReader fs1 = new StreamReader(args[0]);
-            //StreamWriter result1 = new StreamWriter(args[0] + ".usernames");
 
             string s = "";
             var list = new List<string> { };
@@ -40,26 +39,9 @@ namespace Sql2csv
                         result.Close();
 
                     }
-                    //var s1 = s.Split(' ');
-                    //string u = "";
-                    //foreach (var z in s1)
-                    //{
-                    //    result1.WriteLine(z);
-                    //    u += z;
-                    //};
-                    //result1.WriteLine(u);
-                    //u = "";
-                    //foreach (var z in s1.Reverse())
-                    //{
-                    //    u += z;
-                    //};
-                    //result1.WriteLine(u);
+
                 }
             }
-            //result1.Close();
-
-
-
 
         }
     }
