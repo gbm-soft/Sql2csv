a0e98868fac511c9bdf7481d974c17e63f4e30c5
megabspam <megabspam@yandex.ru>
2021-04-24 18:32:33 +0700

    fix

--- Sql2csv/Program.cs
+++ Sql2csv/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Sql2csv
@@ -10,6 +12,55 @@ namespace Sql2csv
     {
         static void Main(string[] args)
         {
+            StreamReader fs1 = new StreamReader(args[0]);
+            //StreamWriter result1 = new StreamWriter(args[0] + ".usernames");
+
+            string s = "";
+            var list = new List<string> { };
+            while (s != null)
+            {
+                s = fs1.ReadLine();
+                if (s != null)
+                {
+                    if (s.Length < 20) continue;
+                    if(s.Substring(0,11)== "INSERT INTO")
+                    {
+                        var idx = s.Substring(13, 100).IndexOf('`'); ;
+                        StreamWriter result = new StreamWriter(args[0] +"."+ s.Substring(13,idx) ,true);
+
+                        string pattern = @"\([^\)]+\)";
+                        Regex rgx = new Regex(pattern);
+                        var m = rgx.Matches(s);
+
+                        foreach (Match match in m)
+                        {
+                            result.WriteLine(match.Value.Substring(1,match.Value.Length-2));
+                        };
+
+                        result.Close();
+
+                    }
+                    //var s1 = s.Split(' ');
+                    //string u = "";
+                    //foreach (var z in s1)
+                    //{
+                    //    result1.WriteLine(z);
+                    //    u += z;
+                    //};
+                    //result1.WriteLine(u);
+                    //u = "";
+                    //foreach (var z in s1.Reverse())
+                    //{
+                    //    u += z;
+                    //};
+                    //result1.WriteLine(u);
+                }
+            }
+            //result1.Close();
+
+
+
+
         }
     }
 }
